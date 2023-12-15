751c94f6ff2f8b2d93743baf9557d73485409dd0
UcheNwuchu <willnwuchu@gmail.com>
2023-12-15 15:37:27 -0800

    tokenize

---
+++ tokenize_cmd.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * tokenize_cmd - to tokenizes line Of Input
+ * @message: input
+ * Return: list of tokenized input
+ */
+
+char **tokenize_cmd(char *message)
+{
+	char **array, *token;
+	int i;
+	char delim[] = {"\n "};
+
+	if (message == NULL)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char *) * BUFSIZE);
+	if (array == NULL)
+	{
+		perror("parse failed!");
+		return (NULL);
+	}
+
+	token = strtok(message, delim);
+
+	i = 0;
+	while (token)
+	{
+		array[i] = token;
+		token = strtok(NULL, delim);
+		i++;
+	}
+	array[i] = NULL;
+
+	return (array);
+
+}
