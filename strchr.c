df33a7a6715e6feb7d2f25653e06e29195fd6982
UcheNwuchu <willnwuchu@gmail.com>
2023-12-15 15:17:52 -0800

    strchr

---
+++ strchr.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * _strchr - to searche the first occurrence of a character
+ * @str: null-terminated string
+ * @c: specified character to search for
+ * Return: the appropriate ointer
+ */
+
+char *_strchr(char *str, char c)
+{
+	int t;
+
+	for (t = 0; str[t] != '\0'; t++)
+	{
+		if (str[t] == c)
+		{
+			return (str + t);
+
+		}
+	}
+
+	if (str[t] == c)
+	{
+		return (str + t);
+	}
+
+	return (0);
+}
