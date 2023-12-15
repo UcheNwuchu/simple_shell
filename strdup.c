8053dc964341831a478c53224a1e23cb68374005
UcheNwuchu <willnwuchu@gmail.com>
2023-12-15 15:25:13 -0800

    strdup.c

---
+++ strdup.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _strdup - to duplicate a string
+ * @str: string to debug
+ */
+
+char *_strdup(char *str)
+{
+	size_t len;
+	char *duplicate;
+
+	len = _strlen(str) + 1;
+	duplicate = malloc(sizeof(char) * len);
+
+	if (duplicate != NULL)
+	{
+		_strcpy(duplicate, str);
+	}
+
+	return (duplicate);
+}
