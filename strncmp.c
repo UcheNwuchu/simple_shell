4a7f32da1c391b2433ae3568d558878c26ed8c06
UcheNwuchu <willnwuchu@gmail.com>
2023-12-15 15:33:23 -0800

    strncmp

---
+++ strncmp.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * _strncmp - To Compare Amount (n) Of Characters Of Two Strings.
+ * @s1: A String.
+ * @s2: A String.
+ * @n: Amount Of Characters To Compare.
+ *
+ * Return: 1 If The Strings Don't Match Otherwise 0
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	if (s1 == NULL)
+		return (-1);
+	for (i = 0; i < n && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
