c01698516bc4fa435688461e54cfcdf4137eb565
UcheNwuchu <willnwuchu@gmail.com>
2023-12-15 14:40:18 -0800

    shell_c

---
+++ shell.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * main -the shell driver code
+ * @ac: The number of command-line arguments
+ * @av: The command-line arguments
+ * Return: to indicates an error
+ */
+int main(__attribute__((unused)) int ac, char **av)
+{
+	char *message, **command;
+
+	if (av[1])
+	{
+		read_file(av[1], av);
+
+	}
+
+	while (true)
+	{
+		if (isatty(STDIN_FILENO))
+			printg("#cisfun$ ");
+		message = _getline();
+		if (message[0] == '\0')
+			continue;
+		command = tokenize_cmd(message);
+
+		if (_strcmp(*command, "exit") == 0)
+		{
+			exit(0);
+		}
+
+		exec_cmd(command, message, av);
+		free(command);
+		free(message);
+	}
+	return (1);
+}
