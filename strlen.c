aacad9a69d5178e8905f6f198e22a2e7182be598
Egbuluese Daniel Emesho <127729223+danniwide1234@users.noreply.github.com>
2023-12-16 02:30:52 +0100

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -4,7 +4,7 @@
 /**
  * _strlen - function that returns the length of a string.
  * @s: pointer parameter passed to the function
- * Return: returns nothings
+ * Return: (void) returns nothing
  */
 
 int _strlen(char *s)
