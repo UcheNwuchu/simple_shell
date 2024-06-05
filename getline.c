d4d9462e3964ab86b0bbf098891df160c04f8cb7
Egbuluese Daniel Emesho <127729223+danniwide1234@users.noreply.github.com>
2024-06-05 15:28:56 +0100

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,48 +1,95 @@
 #include "shell.h"
 
 /**
- * _getline - to read input from standard input
- * Return: returns the input
+ * _realloc - Reallocates a memory block
+ * @ptr: Pointer to the previous memory block
+ * @old_size: Size in bytes of the allocated space for ptr
+ * @new_size: New size in bytes of the new memory block
+ *
+ * Return: Pointer to the new memory block, or NULL if it fails
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *new_ptr;
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return (NULL);
+    }
+    if (!ptr)
+    {
+        return (malloc(new_size));
+    }
+    if (new_size == old_size)
+    {
+        return (ptr);
+    }
+
+    new_ptr = malloc(new_size);
+    if (!new_ptr)
+    {
+        return (NULL);
+    }
+
+    memcpy(new_ptr, ptr, old_size);
+    free(ptr);
+
+    return (new_ptr);
+}
+
+/**
+ * _getline - Reads input from standard input
+ * Return: Returns the input string, or NULL on failure
  */
 char *_getline(void)
 {
-	size_t buffer_size = BUFSIZE, new_buffer_size, r;
-	int cmd = 0;
-	char *buffer = malloc(buffer_size);
+    size_t buffer_size = BUFSIZE;
+    size_t r = 0;
+    ssize_t bytes_read;
+    char *buffer = malloc(buffer_size);
+    char cmd[BUFSIZE];
+    char *temp;
 
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (NULL);
-	}
+    if (buffer == NULL)
+    {
+        return (NULL);
+    }
 
-	r = 0;
-	while (cmd != EOF && cmd != '\n')
-	{
-		fflush(stdin);
-		if (read(STDIN_FILENO, &cmd, 1) == 0)
-		{
-			free(buffer);
-			exit(0);
-		}
+    while (1)
+    {
+        bytes_read = read(STDIN_FILENO, cmd, BUFSIZE);
+        if (bytes_read == -1)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        if (bytes_read == 0)
+        {
+            break;
+        }
 
-		buffer[r] = cmd;
-		if (buffer[0] == '\n')
-		{
-			free(buffer);
-			return ("\0");
-		}
-		new_buffer_size = buffer_size + 1;
+        if (r + bytes_read >= buffer_size)
+        {
+            buffer_size += BUFSIZE;
+            temp = _realloc(buffer, buffer_size - BUFSIZE, buffer_size);
+            if (!temp)
+            {
+                free(buffer);
+                return (NULL);
+            }
+            buffer = temp;
+        }
 
-		if (r >= buffer_size)
-		{
-		buffer = _realloc(buffer, buffer_size, new_buffer_size);
-			if (buffer == NULL)
-			return (NULL);
-		}
-		r++;
-	}
-	buffer[r] = '\0';
-	stringize(buffer);
-	return (buffer);
+        memcpy(buffer + r, cmd, bytes_read);
+        r += bytes_read;
+
+        if (memchr(cmd, '\n', bytes_read))
+        {
+            break;
+        }
+    }
+
+    buffer[r] = '\0';
+    return (buffer);
 }
