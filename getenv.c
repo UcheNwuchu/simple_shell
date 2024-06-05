2cd30f79ae8a0b7044c81c89774bfb0e2a5190f2
Egbuluese Daniel Emesho <127729223+danniwide1234@users.noreply.github.com>
2024-06-05 15:31:47 +0100

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,38 +1,40 @@
 #include "shell.h"
 
 /**
- * _getenv - To Get The Value Of Enviroment Variable By Name
+ * _getenv - Gets the value of an environment variable by name
  * @name: Environment variable name
- * Return: Environment variable value or NULL.
+ * Return: Environment variable value or NULL
  */
 char *_getenv(char *name)
 {
-	int i, t, p = 0;
-	size_t name_len, item_len;
-	char *item;
+    int i, t, p = 0;
+    size_t name_len, item_len;
+    char *item;
 
-	name_len = _strlen(name);
+    name_len = _strlen(name);
 
-	i = 0;
-	while (environ[i])
-	{
+    i = 0;
+    while (environ[i])
+    {
+        if (_strncmp(name, environ[i], name_len) == 0 && environ[i][name_len] == '=')
+        {
+            item_len = _strlen(environ[i]) - name_len - 1;
+            item = malloc(sizeof(char) * (item_len + 1));
+            if (!item)
+            {
+                return (NULL);
+            }
 
+            for (t = name_len + 1; environ[i][t]; t++, p++)
+            {
+                item[p] = environ[i][t];
+            }
+            item[p] = '\0';
 
-		if (_strncmp(name, environ[i], name_len) == 0)
-		{
-			item_len = _strlen(environ[i]) - name_len;
-			item = malloc(sizeof(char) * item_len);
+            return (item);
+        }
+        i++;
+    }
 
-			for (t = name_len + 1; environ[i][t]; t++, p++)
-			{
-				item[p] = environ[i][t];
-			}
-			item[p] = '\0';
-
-			return (item);
-		}
-		i++;
-	}
-
-	return (NULL);
+    return (NULL);
 }
